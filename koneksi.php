cc33a4550f70f4de06c2fdd65168cb2cb3a6b7e0
Salsabilla.A <armadinatasalsabilla@gmail.com>
2026-04-10 20:56:07 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+>?PHP
+$host  = "localhost";
+$user  = "root";
+$password  = "";
+$database  = "pramuka";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+if  (!$conn )  {
+    die ("Koneksi ke database gagal :") . mysqli_connect_error() ) ;
+}
+mysqli_set_charset ($conn, "utf8");
+?>
