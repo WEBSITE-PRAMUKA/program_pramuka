17b421598f1c3733a53dc779a6117f34d1958ec7
MaszAbdil <mohammadabdillahnashi@gmail.com>
2026-04-12 20:25:20 +0700

    Add files via upload

---
+++ pramuka.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 08 Apr 2026 pada 18.30
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pramuka`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nta` varchar(50) NOT NULL,
+  `gugus` varchar(50) DEFAULT NULL,
+  `jk` enum('Laki-laki','Perempuan') DEFAULT NULL,
+  `kontak` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('anggota','admin','bendahara') DEFAULT 'anggota'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `nama`, `nta`, `gugus`, `jk`, `kontak`, `password`, `role`) VALUES
+(10, 'Pak Surya', 'ADMIN001', 'Kwartir Cabang', 'Laki-laki', 'admin@pramuka.id', '0192023a7bbd73250516f069df18b500', 'admin'),
+(11, '', '', '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 'anggota'),
+(12, 'Siti Bendahara', 'BND001', '12-001', 'Perempuan', 'bendahara@pramuka.id', '62f7dec74b78ba0398e6a9f317f55126', 'bendahara'),
+(13, 'Putra Agung', 'AGT001', '12-001', 'Laki-laki', 'ahmad@email.com', '7db5345b01396df74f676a334f74c3d5', 'anggota');
+
+--
+-- Trigger `users`
+--
+DELIMITER $$
+CREATE TRIGGER `before_insert_users` BEFORE INSERT ON `users` FOR EACH ROW BEGIN
+    IF NEW.role = 'admin' AND (SELECT COUNT(*) FROM users WHERE role = 'admin') >= 1 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error: Admin sudah ada!';
+    END IF;
+    
+    IF NEW.role = 'bendahara' AND (SELECT COUNT(*) FROM users WHERE role = 'bendahara') >= 1 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error: Bendahara sudah ada!';
+    END IF;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nta` (`nta`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
