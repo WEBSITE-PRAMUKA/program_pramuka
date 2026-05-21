5f59a25b9beab4eb583a16eb17f3edb6c83ffd80
unknown <mohammadabdillahnashi@gmail.com>
2026-05-21 09:16:55 +0700

    Merubah file proses pada galeri

--- galeri/proses_galeri.php
+++ galeri/proses_galeri.php
@@ -3,18 +3,26 @@ session_start();
 include "../config/koneksi.php";
 
 if ($_SESSION['role'] == 'admin') {
-    $judul = mysqli_real_escape_string($conn, $_POST['judul']);
-    $kategori = $_POST['kategori'];
+    $judul     = mysqli_real_escape_string($conn, $_POST['judul']);
+    $kategori  = mysqli_real_escape_string($conn, $_POST['kategori']);
+    $tanggal   = mysqli_real_escape_string($conn, $_POST['tanggal']);
+    $lokasi    = mysqli_real_escape_string($conn, $_POST['lokasi']);
+    $peserta   = mysqli_real_escape_string($conn, $_POST['peserta']);
+    $deskripsi = mysqli_real_escape_string($conn, $_POST['deskripsi']);
     
     $foto_name = $_FILES['foto']['name'];
-    $foto_tmp = $_FILES['foto']['tmp_name'];
+    $foto_tmp  = $_FILES['foto']['tmp_name'];
     
-    // Beri nama unik agar tidak bentrok
+    // Beri nama unik
     $unique_name = time() . "_" . $foto_name;
-    $path = "uploads/galeri/" . $unique_name;
+    // PENTING: Arahkan ke folder assets agar gambar bisa diload di GaleryLog
+    $path = "../assets/galeri/" . $unique_name;
 
     if (move_uploaded_file($foto_tmp, $path)) {
-        mysqli_query($conn, "INSERT INTO galeri (judul_galeri, kategori, foto_cover) VALUES ('$judul', '$kategori', '$unique_name')");
+        $query = "INSERT INTO galeri (judul_galeri, kategori, foto_cover, tanggal, lokasi, peserta, deskripsi) 
+                  VALUES ('$judul', '$kategori', '$unique_name', '$tanggal', '$lokasi', '$peserta', '$deskripsi')";
+        mysqli_query($conn, $query);
+        
         header("location:../admin/galeri_admin.php");
     }
 }
